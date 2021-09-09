a8f69ad6c34a25d2bfd49f92e2170539ee8347e3
kayac-chang <egg734631@gmail.com>
2021-09-09 20:38:23 +0800

    add default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
-// const colors = require('tailwindcss/colors')
-
 module.exports = {
-  mode: 'jit',
-  purge: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue}'],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  purge: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          lightest: "#FEFEFE",
+          lighter: "#F6F8FF",
+          light: "#697C9A",
+          dark: "#2B3442",
+          darker: "#F6F8FF",
+        },
+
+        blue: {
+          light: "#0079FF",
+          dark: "#4B6A9B",
+          darker: "#1E2A47",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
